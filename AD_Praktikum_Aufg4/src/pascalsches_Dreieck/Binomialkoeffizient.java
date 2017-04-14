542f25988a02d0ffacee134694cbc0564551c17e
Johannes Kruber <johannes.kruber@haw-hamburg.de>
2017-04-14 18:03:40 +0200

    Implement Binomialalgorithmus

--- AD_Praktikum_Aufg4/src/pascalsches_Dreieck/Binomialkoeffizient.java
+++ AD_Praktikum_Aufg4/src/pascalsches_Dreieck/Binomialkoeffizient.java
@@ -8,10 +8,33 @@ package pascalsches_Dreieck;
 public class Binomialkoeffizient
 {
 
+	public static int[] pascalReihe(int n)
+	{
+		int[] pascalzahlen = new int[n + 1];
+
+		for (int k = 0; k <= (n)/2; k++)
+		{
+			pascalzahlen[k] = (int)(fakultät(n) / (fakultät(k) * fakultät(n - k)));
+			pascalzahlen[n - k] = pascalzahlen[k];
+		}
+
+		return pascalzahlen;
+	}
+
+	private static long fakultät(long n)
+	{
+		if (n <= 1)
+			return 1;
+		return n * fakultät(n - 1);
+	}
+
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		int[] pascal = pascalReihe(20);
+		for (int i = 0; i < pascal.length; i++)
+		{
+			System.out.println(pascal[i]);
+		}
 	}
 
 }
